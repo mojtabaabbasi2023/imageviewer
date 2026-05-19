fa1dee9319ea2bd52c6f8a0715c32cff4e4bb9b7
Mojtaba Abbasi <Mabbasi@noornet.net>
2026-05-19 14:29:51 +0330

    edit image address

--- src/playground/App.vue
+++ src/playground/App.vue
@@ -4,70 +4,70 @@ import ImageViewer from '../components/ImageViewer.vue'
 
 const images = ref([
   {
-    path: "@/assets/d-a-z-h-mohammad.jpg",
+    path: "/d-a-z-h-mohammad.jpg",
     width: 1424,
     height: 2208
   },
   {
-    path: "@/assets/d-a-z-h-mohammad.jpg",
+    path: "/d-a-z-h-mohammad.jpg",
     width: 1424,
     height: 2208
   },
   {
-    path: "@/assets/d-a-z-h-mohammad.jpg",
+    path: "/d-a-z-h-mohammad.jpg",
     width: 1424,
     height: 2208
   },
   {
-    path: "@/assets/d-a-z-h-mohammad.jpg",
+    path: "/d-a-z-h-mohammad.jpg",
     width: 1424,
     height: 2208
 
   },
   {
-    path: "@/assets/d-a-z-h-mohammad.jpg",
+    path: "/d-a-z-h-mohammad.jpg",
     width: 1424,
     height: 2208,
 
   },
   {
-    path: "@/assets/d-a-z-h-mohammad.jpg",
+    path: "/d-a-z-h-mohammad.jpg",
     width: 1424,
     height: 2208,
 
   },
   {
-    path: "@/assets/d-a-z-h-mohammad.jpg",
+    path: "/d-a-z-h-mohammad.jpg",
     width: 1424,
     height: 2208,
 
   },
   {
-    path: "@/assets/d-a-z-h-mohammad.jpg",
+    path: "/d-a-z-h-mohammad.jpg",
     width: 1424,
     height: 2208,
 
   },
   {
-    path: "@/assets/d-a-z-h-mohammad.jpg",
+    path: "/d-a-z-h-mohammad.jpg",
     width: 1424,
     height: 2208,
 
   },
   {
-    path: "@/assets/d-a-z-h-mohammad.jpg",
+    path: "/d-a-z-h-mohammad.jpg",
     width: 1424,
     height: 2208,
 
   },
   {
-    path: "@/assets/d-a-z-h-mohammad.jpg",
+    path: "/d-a-z-h-mohammad.jpg",
     width: 1424,
     height: 2208,
 
   },
   {
-    path: "@/assets/d-a-z-h-mohammad.jpg",
+    path: "/d-a-z-h-mohammad.jpg",
     width: 1424,
     height: 2208,
 
